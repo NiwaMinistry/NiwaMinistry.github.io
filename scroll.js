864885c6ecc07b93afebc5178fa840f19674a178
NiwaMinistry <tonewise.music@gmail.com>
2021-04-24 08:43:50 -0500

    fixed scrolling

--- scroll.js
+++ scroll.js
@@ -1,43 +1,80 @@
 
-
-$.fn.is_on_screen = function(){
-     
-  var win = $(window);
+$(document).ready(function(){
+  $(".nav.active-state").click(function(){
+    var delay = 400;
    
-  var viewport = {
-      top : win.scrollTop(),
-      left : win.scrollLeft()
-  };
-  viewport.right = viewport.left + win.width();
-  viewport.bottom = viewport.top + win.height();
-   
-  var bounds = this.offset();
-  bounds.right = bounds.left + this.outerWidth();
-  bounds.bottom = bounds.top + this.outerHeight();
-   
-  return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-   
-};
-
-
-$(window).on("scroll touchstart touchend touchmove resize", function(){ // bind window scroll event
-  
- 
-if( $('.fb-page').length > 0 ) { // if target element exists in DOM
-  if( $('.fb-page').is_on_screen() ) { // if target element is visible on screen after DOM loaded
-  
-   
-
-    
-       $('.fbwrap').addClass('.fvisible');
-      
-      } else {  $('.fbwrap').removeClass('.fvisible');}
-       
-    
-    
-}
+  $('#homecontent').css('display',('block'));
+  setTimeout(function() {
+  $('#gardencontent').css('display',('none'));
+  $('#menus').css('display',('none'));
+  $('#photocontent').css('display',('none'));
+  $('#boutiquecontent').css('display',('none'));
+  $('#locationcontent').css('display',('none'));
+}, delay);
 });
+});
+$(document).ready(function(){
+  $(".nav.other.1").click(function(){
+    $('#gardencontent').css('display',('block'));
+    setTimeout(function() {
+  $('#homecontent').css('display',('none'));
+  $('#menus').css('display',('none'));
+  $('#photocontent').css('display',('none'));
+  $('#boutiquecontent').css('display',('none'));
+  $('#locationcontent').css('display',('none'));
+}, delay);
+});
+});
+$(document).ready(function(){
+  $(".nav.other.2").click(function(){
 
+    $('#menus').css('display',('block'));
+    setTimeout(function() {
+  $('#homecontent').css('display',('none'));
+  $('#gardencontent').css('display',('none'));
+  $('#photocontent').css('display',('none'));
+  $('#boutiquecontent').css('display',('none'));
+  $('#locationcontent').css('display',('none'));
+}, delay);
+});
+});
+$(document).ready(function(){
+  $(".nav.other.3").click(function(){
+    $('#photocontent').css('display',('block'));
+    setTimeout(function() {
+  $('#homecontent').css('display',('none'));
+  $('#gardencontent').css('display',('none'));
+  $('#menus').css('display',('none'));
+  $('#boutiquecontent').css('display',('none'));
+  $('#locationcontent').css('display',('none'));
+}, delay);
+});
+});
+$(document).ready(function(){
+  $(".nav.other.4").click(function(){
+  
+    $('#boutiquecontent').css('display',('block'));
+    setTimeout(function() {
+  $('#homecontent').css('display',('none'));
+  $('#gardencontent').css('display',('none'));
+  $('#menus').css('display',('none'));
+  $('#photocontent').css('display',('none'));
+  $('#locationcontent').css('display',('none'));
+}, delay);
+});
+});
+$(document).ready(function(){
+  $(".nav.other.5").click(function(){
+    $('#locationcontent').css('display',('block'));
+    setTimeout(function() {
+  $('#homecontent').css('display',('none'));
+  $('#gardencontent').css('display',('none'));
+  $('#menus').css('display',('none'));
+  $('#photocontent').css('display',('none'));
+  $('#boutiquecontent').css('display',('none'));
+}, delay);
+});
+});
 
 
 
